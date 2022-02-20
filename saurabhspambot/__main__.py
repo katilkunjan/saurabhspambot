d858d82ede4b6db48f97d1104eab570914d048c5
bhumiharsaurabh <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-02-20 21:47:24 +0530

    saurabhspambot

--- saurabhspambot/__main__.py
+++ saurabhspambot/__main__.py
@@ -26,34 +26,34 @@ print("Saurabh Spam Bot Successfully deployed -!")
 print("Enjoy! Do visit @heartbrokenperson1")
 
 if __name__ == "__main__":
-    Riz.run_until_disconnected()
+    saurabh.run_until_disconnected()
     
 if __name__ == "__main__":
-    Riz2.run_until_disconnected()
+    saurabh2.run_until_disconnected()
 
 if __name__ == "__main__":
-    Riz3.run_until_disconnected()
+    saurabh3.run_until_disconnected()
     
 if __name__ == "__main__":
-    Riz4.run_until_disconnected()
+    saurabh4.run_until_disconnected()
 
 if __name__ == "__main__":
-    Riz5.run_until_disconnected()
+    saurabh5.run_until_disconnected()
     
 if __name__ == "__main__":
-    Riz6.run_until_disconnected()
+    saurabh6.run_until_disconnected()
     
 if __name__ == "__main__":
-    Riz7.run_until_disconnected()
+    saurabh7.run_until_disconnected()
 
 if __name__ == "__main__":
-    Riz8.run_until_disconnected()
+    saurabh8.run_until_disconnected()
     
 if __name__ == "__main__":
-    Riz9.run_until_disconnected()
+    saurabh9.run_until_disconnected()
 
 if __name__ == "__main__":
-    Riz10.run_until_disconnected()   
+    saurabh10.run_until_disconnected()   
     
 if __name__ == "__main__":
     saurabh11.run_until_disconnected()
